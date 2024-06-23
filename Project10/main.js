081432db382d2326670c6a6cc8ef57dfa6be3185
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-23 12:03:05 +0900

    休憩タイムの実装した

--- Project10/main.js
+++ Project10/main.js
@@ -2,9 +2,12 @@ const startEl = document.getElementById('start');
 const stopEl = document.getElementById('stop');
 const resetEl = document.getElementById('reset');
 const timerEl = document.getElementById('timer');
+const breakModal = document.getElementById('breakModal');
+const closeModal = document.getElementById('closeModal');
 
 let interval;
-let timeLeft = 1500;
+let timeLeft = 10;
+let isBreakTime = false;
 
 function updateTimer() {
     let minutes = Math.floor(timeLeft / 60);
@@ -12,7 +15,7 @@ function updateTimer() {
     let formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 
     timerEl.innerHTML = formattedTime
-}
+};
 
 function startTimer() {
     interval = setInterval(() => {
@@ -20,19 +23,47 @@ function startTimer() {
         updateTimer();
         if (timeLeft === 0) {
             clearInterval(interval);
-            alert("Time's up !!");
-            timeLeft = 1500;
+            if (isBreakTime) {
+                alert('Break is over, back to work');
+                timeLeft = 1500;
+                isBreakTime = false;
+            } else {
+                breakModal.classList.add("show");
+                timeLeft = 10;
+                isBreakTime = true;
+            }
+            startTimer();
         }
     }, 1000);
-}
+};
+
 function stoptTimer() {
     clearInterval(interval);
-}
+};
+
 function resetTimer() {
     clearInterval(interval);
     timeLeft = 1500;
     updateTimer();
-}
+};
+
+closeModal.onclick = function () {
+    breakModal.classList.remove("show");
+    isBreakTime = false; // モーダルが閉じられたら休憩モードをリセット
+};
+
+window.onclick = function (event) {
+    if (event.target === breakModal) {
+        breakModal.classList.remove("show");
+        isBreakTime = false; // モーダルが閉じられたら休憩モードをリセット
+    }
+};
+
+// ページリロード時にモーダルが表示されないようにする
+window.onload = function () {
+    breakModal.classList.remove("show");
+    isBreakTime = false;
+};
 
 startEl.addEventListener('click', startTimer);
 stopEl.addEventListener('click', stoptTimer);
